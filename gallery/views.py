77b81a949d3b069331cb58084fd0f830680c983f
Matt McCarthy <matt@mccarthywebdesign.com>
2020-04-09 17:08:43 -0500

    Added gallery index; various styling changes

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,28 @@
+from datetime import datetime
+
 from django.shortcuts import render
-from django.http import (
-    HttpResponse,
-    # HttpResponseBadRequest,
-    # HttpResponseNotFound,
-)
+from django.http import HttpResponseBadRequest
+
+from .models import GalleryImage
+from skka.settings import TZ, NAME
 
 def index(request):
-    return HttpResponse('Gallery')
+    if request.method != 'GET':
+        return HttpResponseBadRequest()
+
+    images_by_category = []
+    for category_id, name in GalleryImage.CATEGORY_CHOICES:
+        images = GalleryImage.objects.filter(category=category_id)
+
+        if images:
+            images_by_category.append({
+                'id': category_id,
+                'name': name,
+                'images': images,
+            })
+
+    return render(request, 'gallery/index.html', {
+        'images_by_category': images_by_category,
+        'year': datetime.now(TZ).year,
+        'name': NAME,
+    })
