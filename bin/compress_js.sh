876c15a469e9835ddbe2cea19f57088201b8f7ba
Matt McCarthy <matt@mccarthywebdesign.com>
2020-04-13 19:03:58 -0500

    Updated gallery manager to include reordering and delete buttons

--- bin/compress_js.sh
+++ bin/compress_js.sh
@@ -11,10 +11,14 @@ js_output_carousel="home/static/home/js/carousel.min.js"
 js_input_carousel_manager="home/static/home/js/carousel_manager.js"
 js_output_carousel_manager="home/static/home/js/carousel_manager.min.js"
 
+js_input_gallery="gallery/static/gallery/js/gallery.js"
+js_output_gallery="gallery/static/gallery/js/gallery.min.js"
+
 declare -a commands=(
   # "java -jar $closure_compiler --js $js_input_global --js_output_file $js_output_global"
   # "java -jar $closure_compiler --js $js_input_carousel --js_output_file $js_output_carousel"
   # "java -jar $closure_compiler --js $js_input_carousel_manager --js_output_file $js_output_carousel_manager"
+  # "java -jar $closure_compiler --js $js_input_gallery --js_output_file $js_output_gallery"
 )
 
 for i in "${commands[@]}"; do
---
+++ gallery/static/gallery/js/gallery.js
@@ -0,0 +1,65 @@
+$(document).ready(() => {
+  // reorder images
+  let $up = $('.image-list .controls :first-child');
+  let $down = $('.image-list .controls :last-child');
+
+  $up.click(function () {
+    let $li = $(this).parents('li');
+
+    if (!$li.is(':first-of-type')) {
+      let $prev = $li.prev();
+
+      $li.animate({'bottom': `${$prev.outerHeight() + 8}px`}, 500, function () {
+        $(this).after($prev.detach()).prop('style', '');
+      });
+
+      $prev.animate({'top': `${$li.outerHeight() + 8}px`}, 500, function () {
+        $(this).prop('style', '');
+      });
+    }
+  });
+
+  $down.click(function () {
+    let $li = $(this).parents('li');
+
+    if (!$li.is(':last-of-type')) {
+      let $next = $li.next();
+
+      $li.animate({'top': `${$next.outerHeight() + 8}px`}, 500, function () {
+        $(this).before($next.detach()).prop('style', '');
+      });
+
+      $next.animate({'bottom': `${$li.outerHeight() + 8}px`}, 500, function () {
+        $(this).prop('style', '');
+      });
+    }
+  });
+
+  // submit changes
+  $('#submit').click(function () {
+    let orders = [];
+
+    $('.image-list').each(function () {
+      let order = [];
+
+      $('li', this).each(function () {
+        order.push($(this).data('id'));
+      });
+
+      orders.push({
+        'category_id': $(this).data('id'),
+        'order': order,
+      })
+    });
+
+    $.ajax('/gallery/reorder/', {
+      'data': {'order': JSON.stringify(orders)},
+      'success': function (data) {
+        window.location.reload(false);
+      },
+      'error': function (data) {
+        window.location.reload(false);
+      },
+    });
+  });
+});
