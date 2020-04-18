84400de106c0c2519d54b0f4a29c4dfb07913042
Matt McCarthy <matt@mccarthywebdesign.com>
2020-04-18 21:45:36 +0000

    Switched master to production mode; added allowed hosts

--- skka/settings.py
+++ skka/settings.py
@@ -24,14 +24,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(ns_7t65z4v)#*bge9=sf!2fysc2yciw)z)r0z6$^x0u!xk@+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = [
         '10.0.0.100',
     ]
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = [
+        'localhost',
+        '161.35.61.106',
+        'skkeganlandscapes.com',
+        'www.skkeganlandscapes.com',
+    ]
 
 
 # Application definition
