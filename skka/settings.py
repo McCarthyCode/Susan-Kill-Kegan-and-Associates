d92d02bef7290b801f4b8c46479d7503521b1e63
Matt McCarthy <matt@mccarthywebdesign.com>
2020-04-19 21:13:08 +0000

    Updated PGPASS in settings.py

--- skka/settings.py
+++ skka/settings.py
@@ -100,7 +100,7 @@ else:
     PGPASSWORD_FILE = '%s/auth/.pgpass' % BASE_DIR
     with open(PGPASSWORD_FILE, 'r', encoding='utf8') as f:
         content = f.readline()
-    PGPASSWORD = content[12:-1]
+    PGPASSWORD = content[14:-1]
 
     DATABASES = {
         'default': {
